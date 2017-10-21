8e38e7c09780c91f427a01c3c2fdcb985049650d
vag-tsdc <30638922+vag-tsdc@users.noreply.github.com>
2017-10-21 12:27:36 -0400

    timer, toggle buttons

--- triviagame/assets/trivia.js
+++ triviagame/assets/trivia.js
@@ -1,29 +1,96 @@
-/*    TRIVIA GAME       */
-
-
-
-/*    TRIVIA GAME       */
+/*    global variables       */
 
 
 
 
+/* pre game */
 
-/*    TRIVIA GAME       */
-
-
-
-
-
-/*    TRIVIA GAME       */
-
-
-
-
-
-/*    TRIVIA GAME       */
-
+function ageCheck() {
+    var ageCheck = confirm("Are you at least 16 years old?")
+    if (ageCheck) {
+        alert("Let's talk about sex!")
+    }
+    else {
+        alert("nope.")
+        $("body").text("Nope. Try again when you're older.")
+    }
+    console.log("Age Check: " + ageCheck);
+}
 
 
 
+/* game variables */
 
-/*    TRIVIA GAME       */
+
+var timer;
+
+var triviaKey = [];
+
+var userAnswer = [];
+
+
+
+
+
+/*    CURRENT STATS       */
+var questionsLeft = "#current-question-rem";
+
+
+
+
+
+
+
+/*    User Input       */
+$(document).ready(function() {
+
+// confirm user Age 
+ageCheck();
+
+
+
+// start button
+ // .on("click") for #start button
+ $("#start-button").on("click", function() {
+    //  Set the button alert's timeout to run 3 seconds after the function's called.
+    startQuizTimer = setTimeout(function() {
+      alert("Time's Up");
+    }, 150000);
+  });
+    
+
+// var currentUserInput = {
+
+
+
+
+
+
+
+// var triviaKey = {
+//     q1text : " ",
+//     q1rightanswer : " "
+
+// }
+
+// preTrivia();
+
+
+
+// $("#start").on("click", function() {
+//     // start timer
+    
+//   });       
+
+  /* timer timer timer */
+
+// function timer() {
+//     $("#time-rem").html();
+// }
+
+
+// /*   GAME OVER      */
+// function getUserAnswers() {
+
+
+})
